e74654616d135835bde80a9bbe2dd5ef872328f8
Felipe Pereira <felipe.psantos0411@gmail.com>
2020-09-18 20:26:20 -0300

    alteracoes na tela

--- lib/app/ui/quiz/quiz_page.dart
+++ lib/app/ui/quiz/quiz_page.dart
@@ -5,130 +5,112 @@ import 'package:covid_app/app/ui/quiz/get_uf_viewmodel.dart';
 import 'package:covid_app/app/ui/quiz/quiz_page_viewmodel.dart';
 import 'package:covid_app/app/widgets/button_component.dart';
 import 'package:covid_app/core/constants/colors.dart';
+import 'package:covid_app/core/constants/dimens.dart';
 import 'package:flutter/material.dart';
 
 class QuizPage extends StatelessWidget {
   QuizPageViewModel vm = QuizPageViewModel(GeolocatorService(),
       GeocodingService(), CovidApiRepository(), GetUfViewModel());
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _body(context, vm),
-    );
+    return _body(context, vm);
   }
 }
 
-Widget _body(BuildContext context, QuizPageViewModel vm) {
+Widget _body(context, vm) {
   return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
     children: <Widget>[
       FutureBuilder(
         future: vm.getData(),
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Container(
-              margin: EdgeInsets.all(5),
-              padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(50, 92, 127, 0.8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.35,
+                color: Color.fromRGBO(50, 92, 127, 0.9),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    strokeWidth: 5,
+                  ),
+                ),
+              );
+            default:
+              return Column(
                 children: <Widget>[
-                  _text(title: "${snapshot.data.state}", color: Colors.white),
-                  _underlineBlackGradient(),
-                  _row(
-                      labelTextTitle: "Casos",
-                      dataTextTitle: "${snapshot.data.cases}"),
-                  _underlineBlackGradient(),
-                  _row(
-                      labelTextTitle: "Suspeitos",
-                      dataTextTitle: "${snapshot.data.suspects}"),
-                  _underlineBlackGradient(),
-                  _row(
-                      labelTextTitle: "Mortos",
-                      dataTextTitle: "${snapshot.data.deaths}"),
-                  _underlineBlackGradient(),
-                  Align(
-                    alignment: Alignment.center,
-                    child: _text(
-                        title: "${snapshot.data.datetime}",
-                        fontSize: 15,
-                        color: Colors.white),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.35,
+                    color: Color.fromRGBO(50, 92, 127, 0.9),
+                    child: Column(
+                      children: <Widget>[
+                        _text(
+                            title:
+                                "${snapshot.data.state}, ${snapshot.data.uf}",
+                            fontSize: twentyFour),
+                        _underlineBlackGradient(),
+                        _row(title1: "Casos", title2: "${snapshot.data.cases}"),
+                        _underlineBlackGradient(),
+                        _row(
+                            title1: "Suspeitos",
+                            title2: "${snapshot.data.suspects}"),
+                        _underlineBlackGradient(),
+                        _row(
+                            title1: "Mortos",
+                            title2: "${snapshot.data.deaths}3"),
+                        _underlineBlackGradient(),
+                      ],
+                    ),
                   ),
                 ],
-              ),
-            );
-          } else {
-            return Container(child: Text("Erro"));
+              );
           }
         },
       ),
-      ButtonComponent(
-        fillColor: darkPrimaryColor,
-        textColor: Colors.white,
-        title: 'Quiz',
-        loginFun: () {},
+      Divider(
+        height: 200,
+      ),
+      Container(
+        child: ButtonComponent(
+          title: "Quiz",
+          fillColor: rosePrimaryColor,
+          textColor: Colors.black,
+          loginFun: () {},
+        ),
       ),
     ],
   );
 }
 
-//
-
-//
-
-Row _row({
-  String labelTextTitle = "default",
-  double labelTextFontSize = 20,
-  Color labelTextColor = Colors.white,
-  String dataTextTitle = "default",
-  double dataTextFontSize = 20,
-  Color dataTextColor = Colors.white,
-}) {
-  return Row(
-    children: <Widget>[
-      _labelText(
-          title: labelTextTitle,
-          fontSize: dataTextFontSize,
-          color: dataTextColor),
-      Spacer(),
-      _dataText(
-          title: dataTextTitle,
-          fontSize: labelTextFontSize,
-          color: labelTextColor),
-    ],
+_text({String title, double fontSize, Color color = Colors.white}) {
+  return Container(
+    margin: EdgeInsets.only(top: 10, bottom: 10),
+    child: Text(
+      title,
+      style: TextStyle(fontSize: fontSize, color: color),
+    ),
   );
 }
 
-Text _labelText(
-    {String title = "default",
-    double fontSize = 25,
-    Color color = Colors.black}) {
-  return Text(
-    title,
-    style: TextStyle(fontSize: fontSize, color: color),
-  );
-}
-
-Text _dataText(
-    {String title = "default",
-    double fontSize = 20,
-    Color color = Colors.black}) {
-  return Text(
-    title,
-    style: TextStyle(fontSize: fontSize, color: color),
-  );
-}
-
-Text _text(
-    {String title = "default",
-    double fontSize = 30,
-    Color color = Colors.black}) {
-  return Text(
-    title,
-    style: TextStyle(fontSize: fontSize, color: color),
-  );
+_row(
+    {String title1,
+    String title2,
+    double fontSize1 = twenty,
+    double fontSiz2 = eighteen,
+    Color color1 = Colors.white,
+    Color color2 = Colors.white}) {
+  return Row(children: <Widget>[
+    _text(title: title1, fontSize: fontSize1, color: color1),
+    Spacer(),
+    _text(title: title2, fontSize: fontSiz2, color: color2),
+  ]);
 }
 
 SizedBox _underlineBlackGradient() {
@@ -141,11 +123,11 @@ SizedBox _underlineBlackGradient() {
           begin: Alignment(1.0, 0.0),
           end: Alignment(-1.0, 0.0),
           colors: [
-            Color.fromRGBO(00, 00, 00, 0.2),
-            Color.fromRGBO(00, 00, 00, 0.5),
-            Color.fromRGBO(00, 00, 00, 1),
-            Color.fromRGBO(00, 00, 00, 0.5),
-            Color.fromRGBO(00, 00, 00, 0.2),
+            Color.fromRGBO(255, 255, 255, 0.2),
+            Color.fromRGBO(213, 213, 213, 0.5),
+            Color.fromRGBO(184, 184, 184, 0.8),
+            Color.fromRGBO(213, 213, 213, 0.5),
+            Color.fromRGBO(255, 255, 255, 0.2),
           ],
         ),
       ),
