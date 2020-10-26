74c3309f94a25585db05eac406525c70f39c8a8f
Felipe Pereira <felipe.psantos0411@gmail.com>
2020-10-26 18:41:09 -0300

    add GoogleMap widget in health container

--- lib/app/ui/containers/health/health_container.dart
+++ lib/app/ui/containers/health/health_container.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // ignore: must_be_immutable
-class HealthContainer extends StatelessWidget {
+class HealthContainer extends StatefulWidget {
   Color color;
 
   HealthContainer({this.color});
+
+  @override
+  _HealthContainerState createState() => _HealthContainerState();
+}
+
+class _HealthContainerState extends State<HealthContainer> {
+  GoogleMapController mapController;
+  double latitude = -23.5570497;
+  double longitude = -46.4922667;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: color,
+      child: GoogleMap(
+        myLocationEnabled: true,
+        initialCameraPosition:
+            CameraPosition(target: LatLng(latitude, longitude), zoom: 13.0),
+      ),
     );
   }
 }
