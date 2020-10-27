1456b8afba811645823036b2e44362e803751460
Felipe Pereira <felipe.psantos0411@gmail.com>
2020-10-27 16:13:43 -0300

    add method getData

--- lib/app/ui/containers/health/health_container_viewmodel.dart
+++ lib/app/ui/containers/health/health_container_viewmodel.dart
@@ -1,1 +1,15 @@
-class HealthContainerViewModel {}
+import 'package:covid_app/app/model/device_position_model.dart';
+import 'package:covid_app/app/service/geolocator/geolocator_service.dart';
+import 'package:get_it/get_it.dart';
+
+class HealthContainerViewModel {
+  final geolocatorService = GetIt.I.get<GeolocatorService>();
+  DevicePositionModel model;
+
+  Future time() async {}
+  Future<DevicePositionModel> getData() async {
+    await Future.delayed(Duration(seconds: 8), () {});
+    model = await geolocatorService.getDevicePosition();
+    return model;
+  }
+}
